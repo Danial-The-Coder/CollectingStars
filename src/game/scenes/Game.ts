afc80fa862a31233de0d29aa3ad7d89c920ac617
Danial <danialrezk12@gmail.com>
2026-06-28 18:40:31 +0300

    following the LMS

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -2,14 +2,24 @@ import { Scene } from 'phaser';
 
 export class Game extends Scene
 {
+  platforms: string[];
+  
     constructor ()
     {
         super('Game');
+        this.platforms = []
+        this.player = undefined;
+        this.stars= undefined;
     }
+    
 
+    init() {
+    this.platforms = []; 
+    this.player = undefined;
+    this.stars= undefined
+  }
     preload ()
     {
-        // this.load.setPath('assets');
 
       this.load.image('background', './src/assets/sky.png');
       this.load.image('ground', './src/assets/platform.png');
@@ -26,6 +36,22 @@ export class Game extends Scene
 
     create ()
     {
+      this.stars = this.physics.add.group({
+        key: "star",
+        repeat: 10, 
+        setXY: { x: 50, y: 0, stepX: 70 },
+      });
+      this.stars.children.iterate(function (child) {
+      child.setBounceY(0.5); 
+      });
+      this.player = this.physics.add.sprite(100, 450, "dude");
+      this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+      this.platforms=this.physics.add.staticGroup()
+      this.platforms.create(600, 400, "ground");
+      this.platforms.create(50, 250, "ground");
+      this.platforms.create(750, 220, "ground");  
       this.add.image(400, 300, 'sky');
+      this.player.setCollideWorldBounds(true);
+      this.physics.add.collider(this.stars, this.platforms);
     }
 }
